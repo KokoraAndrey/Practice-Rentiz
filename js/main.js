a59cd084cabc11839091ae79ea914e7269fcfed6
Andrey Kokora <andrey18121996@gmail.com>
2023-04-25 07:49:01 +0300

    create perfect block + adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
+const filter = document.querySelector('.filter');
 
-burger.addEventListener('click', () => {
+if (burger && menu) {
+    burger.addEventListener('click', getBurger);
+}
+
+if (filter) {
+    const items = document.querySelectorAll('.block-filter');
+
+    items.forEach(item => {
+        item.addEventListener('click', event => {
+            item.querySelector('.block-filter__dropdown').classList.toggle(
+                'active'
+            );
+            item.querySelector('.block-filter__icon').classList.toggle(
+                'active'
+            );
+
+            if (event.target.classList.contains('block-filter__item')) {
+                item.querySelector('.block-filter__value').textContent =
+                    event.target.textContent;
+            }
+        });
+    });
+}
+
+function getBurger() {
     burger.classList.toggle('active');
     menu.classList.toggle('open');
-});
+    document.querySelector('body').classList.toggle('lock');
+}
